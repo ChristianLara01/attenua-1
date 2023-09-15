d50bca73e95b2a7692f2f70bfb9a4c6feb590a91
BytebioAtualle <79318235+BytebioAtualle@users.noreply.github.com>
2023-09-15 11:41:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import mercadopago
+import paho.mqtt.client as mqtt
+
+# MQTT Configuration
+MQTT_BROKER_HOST = "mqtt.eclipseprojects.io"
+MQTT_BROKER_PORT = 1883
+MQTT_TOPIC = "estado"
+
+# Function to send MQTT message
+def send_mqtt_message(payload):
+    client = mqtt.Client()
+    client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
+    client.publish(MQTT_TOPIC, payload)
+    client.disconnect()
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Attenua!'
 
 @app.route('/webhook', methods=['POST'])
 def mercado_pago_webhook():
@@ -8,8 +26,9 @@ def mercado_pago_webhook():
     webhook_data = request.json
 
     # Print the JSON data to the terminal
-    print("Received POST request from Mercado Pago:")
+    print("Pagamento recebido de Mercado Pago:")
     print(webhook_data)
+    send_mqtt_message("Pagou"):
 
     # Optionally, you can process the webhook data here as needed
 
