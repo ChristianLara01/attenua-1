9b2ad68013c41a3d6e239f9bc2d5eb5e57cc1eaa
ChristianLara01 <38927368+ChristianLara01@users.noreply.github.com>
2023-09-28 21:04:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ api_key = "AIzaSyCuzKLRuerHBHR9ArHvJm5HzpD7E_Ap170"
 @app.route('/produtos')
 def produtos():
     return render_template('produtos.html')
+    
 @app.route('/')
-def in():
-    return render_template('hi')
+def home():
+    return 'hi'
     
 @app.route('/home/<json_data>', methods=['POST', 'GET'])
-def home(json_data):
+def homedata(json_data):
     try:
         data = jsonify(eval(json_data))
         print(data)
