3c4e24682840de55640019eafce6979056593622
ChristianLara01 <38927368+ChristianLara01@users.noreply.github.com>
2023-10-13 16:45:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,126 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response
 import requests
-import paho.mqtt.client as mqtt
+import json
+import datetime
+import os
+import mercadopago
+import secrets
 
-# MQTT Configuration
-MQTT_BROKER_HOST = "mqtt.eclipseprojects.io"
-MQTT_BROKER_PORT = 1883
-MQTT_TOPIC = "estado"
-
-# Function to send MQTT message
-def send_mqtt_message(payload):
-    client = mqtt.Client()
-    client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
-    client.publish(MQTT_TOPIC, payload)
-    client.disconnect()
+MERCADO_PAGO_ACCESS_TOKEN = "APP_USR-698417925527845-042300-824e07ad45574df479088eebe0fad53c-726883686"
+# Configure sua chave de acesso ao Mercado Pago
+mp = mercadopago.SDK(MERCADO_PAGO_ACCESS_TOKEN)
 
 app = Flask(__name__)
 
-# Sua chave da API do Mercado Pago
-MERCADO_PAGO_ACCESS_TOKEN = 'APP_USR-698417925527845-042300-824e07ad45574df479088eebe0fad53c-726883686'
-# Sua chave da API do Google Maps
-api_key = "AIzaSyCuzKLRuerHBHR9ArHvJm5HzpD7E_Ap170"
+# Caminho para o arquivo JSON de cabines
+cabins_file = os.path.join(os.path.dirname(__file__), 'data', 'cabins.json')
 
-@app.route('/produtos')
-def produtos():
-    return render_template('produtos.html')
+# Função para carregar os dados do JSON
+def load_cabins():
+    with open(cabins_file, 'r') as file:
+        cabins = json.load(file)
+    return cabins
+
+@app.route('/data/cabins.json')
+def get_cabins_data():
+    try:
+        # Carregue o conteúdo do arquivo JSON
+        with open('data/cabins.json', 'r') as json_file:
+            data = json_file.read()
+        # Transforme o conteúdo JSON em um objeto Python (dicionário)
+        data_dict = json.loads(data)
+        return jsonify(data_dict)
+    except FileNotFoundError:
+        # Se o arquivo não for encontrado, retorne uma resposta 404 (Not Found)
+        return "Arquivo não encontrado", 404
+    except Exception as e:
+        # Se ocorrer um erro inesperado, retorne uma resposta 500 (Internal Server Error)
+        return str(e), 500
     
 @app.route('/')
-def home():
-    return "oi"  # Esta string será exibida no navegador quando você acessar a rota "/"
-    
-@app.route('/home/<json_data>', methods=['POST'])
-def homedata(json_data):
-    try:
-        # O parâmetro json_data conterá o JSON na URL
-        # Vamos imprimir este JSON no console
-        print("JSON recebido:", json_data)
+def catalog():
+    cabins = load_cabins()
+    return render_template('catalog.html', cabins=cabins)
 
-        # Se você quiser retornar uma resposta HTTP com o JSON, você pode fazer isso também
-        return json_data
-    except Exception as e:
-        return str(e), 400  # Retorna um código de status 400 (Bad Request) em caso de erro
+@app.route('/reserve/<int:cabin_id>')
+def reserve(cabin_id):
+    return render_template('reservation.html', cabin_id=cabin_id)
 
-@app.route('/mapa')
-def mapa():
-    produto_selecionado = request.args.get('produto')
-    # Lógica para determinar o ponto no mapa com base no produto selecionado
-    # Você pode usar um dicionário ou outra estrutura de dados para mapear produtos para coordenadas
+@app.route('/agendar/<dia>')
+def agendar(dia):
+    # Aqui você pode usar o parâmetro "dia" para exibi-lo na tela ou realizar outras ações necessárias
+    return f'Dia selecionado: {dia}'
 
-    # Suponha que você tenha um dicionário que mapeie produtos para coordenadas
-    coordenadas = {
-        'produto1': {"lat": -25.503600050960685, "lng": -49.16696527260166},
-        'produto2': {"lat": ...},
-        'produto3': {"lat": ...},
-        'produto4': {"lat": ...},
-        'produto5': {"lat": ...},
+
+@app.route('/<cabinId>/<clickedHour>', methods=['GET'])
+def pagamento(cabinId, clickedHour):
+
+    add_appointment(cabinId, clickedHour)
+
+    # Informações do produto
+    print(cabinId)
+    print(clickedHour)
+    produto_nome = "CABINE " +  cabinId + " " + clickedHour
+    produto_preco = 0.02
+
+    # Crie uma preferência de pagamento
+    preference_data = {
+        "items": [
+            {
+                "title": produto_nome,
+                "quantity": 1,
+                "currency_id": "BRL",
+                "unit_price": produto_preco
+
+            }
+        ],
+        "back_urls": {
+            "success": "http://seusite.com/sucesso",
+            "failure": "http://seusite.com/erro",
+            "pending": "http://seusite.com/pendente"
+        }
     }
 
-    ponto = coordenadas.get(produto_selecionado)
+    preference = mp.preference().create(preference_data)
 
-    if ponto:
-        return render_template('mapa.html', ponto=ponto, api_key=api_key)
-    else:
-        return "Produto não encontrado", 404
+    # Redirecione o usuário para a página de pagamento do Mercado Pago
+    return redirect(preference['response']['init_point'])
+
+# Save the JSON data back to the file
+def save_cabins_data(cabins_data):
+    with open('data/cabins.json', 'w') as file:
+        json.dump(cabins_data, file, indent=4)
+
+def add_appointment(cabinId, clickedHour):
+    # Parse the JSON data
+    cabins_data = load_cabins()
+
+    # Find the cabin with the given ID
+    for cabin in cabins_data:
+        if str(cabin['id']) == cabinId:
+            # Extract the date and hour from clickedHour
+            date, hour = clickedHour.split(' ')
+            password = secrets.token_hex(3)
+
+            # Create a new appointment dictionary
+            new_appointment = {
+                'dia': date,
+                'hora': hour,
+                'qtde_horas': 1,  # You can set the duration as needed
+                'id_usuario': 1,  # Replace with the user ID
+                'senha_unica': password,  # Replace with a generated password
+                'pagamento': False  # Replace with a generated password
+            }
+
+            # Add the new appointment to the cabin's appointments
+            cabin['agendamentos'].append(new_appointment)
+
+            # Save the updated JSON data
+            save_cabins_data(cabins_data)
+
+            return 'Appointment added successfully'
+
+    return 'Cabin not found'
 
 # Rota para receber a webhook do Mercado Pago
 @app.route('/webhook', methods=['POST'])
@@ -84,10 +143,9 @@ def webhook():
             payment_data = response.json()
             payment_status = payment_data.get('status')
             payment_id = payment_data.get('id')
-            if(payment_data.get('collection', {}).get('external_reference') == 'ca1' and payment_data.get('collection', {}).get('status') == 'approved'):
-                send_mqtt_message("1")
-            print(payment_data)
-            
+            if(payment_data.get('collection', {}).get('status') == 'approved'):
+                if(payment_data.get('collection', {}).get('reason') == 'CABINE 1 14-10-2023 09:00'):
+                    print(payment_data)            
             if payment_status == 'approved':
                 print("\n\naprovou\n\n")
                 pass
@@ -100,6 +158,7 @@ def webhook():
             return jsonify({'status': 'error', 'message': 'Resource URL not found'}), 500
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
